31ab8c651a0ed9a11b3084108308f60b18c80f50
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2020-09-29 22:39:44 -0300

    Primeiro Commit - Usando Exemplo da Microsoft (Quickstart: Extract text using the Computer Vision 3.0 REST API Read operation and C#)

---
+++ OCRComparer/OCRComparer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OCRComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
